bb8b4e7a84b3639895f643b5614cb760ac513800
Josh Mack <mack8.jo@gmail.com>
2025-10-30 09:40:24 -0400

    feat(backend): add initial hono backend with dummy endpoints

---
+++ backend/src/index.ts
@@ -0,0 +1,23 @@
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+
+const app = new Hono();
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!');
+});
+
+app.get('/sample/:id', (c) => {
+  const { id } = c.req.param();
+  return c.text(`You entered id: ${id}`);
+});
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
